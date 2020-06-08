1613194e4ea3733f06b889e1d520cd81e586ba80
Leandro Aranda <learanda@hotmail.com.ar>
2020-06-08 19:14:52 -0300

    Subo el maquetado inicial

---
+++ app/ingresar.php
@@ -0,0 +1,33 @@
+<?php
+include_once ("header.php")
+?>
+<link rel="stylesheet" href="css/ingresar.css">
+
+            <div class="content-general-inicio">
+                <div class="content-inicio-crear">
+                    <a class="inicio" href="#">INICIO DE SESIÓN</a>
+                    <a class="crear" href="registro.php">CREAR UNA CUENTA</a>
+                </div>
+
+                <div class="content-form-inicio">
+                    <form action="#" method="POST">
+                        <input class="in-inp" type="email" id="email" placeholder="Dirección de correo electrónico" name="email">
+                        <input class="in-inp" type="password" id="password" placeholder="Contraseña" name="password">
+                        <button class="ingreso-button" type="submit">INGRESAR</button>
+                    </form>
+                </div>
+
+                <hr>
+
+                <div class="content-gmail-button">
+                    <button class="ingreso-gmail" type="submit">INGRESAR CON GMAIL</button>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+<?php include_once ("footer.php")?>
---
+++ app/secciones.php
@@ -0,0 +1,21 @@
+<?php
+include_once ("header.php")
+?>
+<link rel="stylesheet" href="css/secciones.css">
+
+            <div class="content-sections">
+                <button class="section-button" type="button">DEPORTES</button>
+                <button class="section-button" type="button">ESPECTACULOS</button>
+                <button class="section-button" type="button">ECONOMÍA</button>
+                <button class="section-button" type="button">POLÍTICA</button>
+                <button class="section-button" type="button">SOCIEDAD</button>
+                <button class="section-button" type="button">MUNDO</button>
+                <button class="section-button" type="button">CULTURA</button>
+                <button class="section-button" type="button">POLICIALES</button>
+            </div>
+
+
+        </div>
+
+
+<?php include_once ("footer.php")?>

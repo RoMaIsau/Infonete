403f816c9643239c2aa2763b4631e7b5ffa9e2e4
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-07 22:29:11 -0300

    Agrega enrutador que se encarga de invocar las acciones de los controladores

---
+++ app/Router.php
@@ -0,0 +1,24 @@
+<?php
+class Router {
+    public static function ejecutarAccionDeControlador($inicializadorDeModulos, $modulo, $accion) {
+        $controlador = self::crearControlador($inicializadorDeModulos, $modulo);
+        self::ejecutarAccion($controlador, $accion);
+    }
+
+    private static function crearControlador($inicializadorDeModulos, $modulo) {
+        $nombreDeControlador = self::crearNombreDeControladorParaModulo($modulo);
+        $controladorValido = method_exists($inicializadorDeModulos, $nombreDeControlador) ? $nombreDeControlador : "crearControladorDefault";
+        $controlador = call_user_func(array($inicializadorDeModulos, $controladorValido));
+        return $controlador;
+    }
+
+    private static function crearNombreDeControladorParaModulo($modulo){
+        return "crear" . "Controlador" . ucfirst($modulo);
+    }
+
+    private static function ejecutarAccion($controlador, $accion){
+        $accionValida = method_exists($controlador, $accion) ? $accion : "index";
+        call_user_func(array($controlador, $accionValida));
+    }
+}
+?>

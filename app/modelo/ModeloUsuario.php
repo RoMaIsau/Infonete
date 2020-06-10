a85879779f9c817bf61be45a768a271695d977da
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-10 09:28:10 -0300

    Se crea el modelo usuario con algunas validaciones para el registro

--- app/modelo/ModeloUsuario.php
+++ app/modelo/ModeloUsuario.php
@@ -8,13 +8,38 @@ class ModeloUsuario {
     }
 
     public function buscarPorCorreoYContrasenia($correo, $password) {
-        return $this->conexion->query("SELECT * FROM Usuario WHERE correo = '$correo' AND contrasena = '$password'");
+        $contraseniaEncriptada = md5($password);
+        $usuario = $this->conexion->query("SELECT * FROM Usuario WHERE correo = '$correo' AND contrasenia = '$contraseniaEncriptada'");
+
+        return $usuario;
     }
 
-    public function registrar($nombre, $apellido, $usuario, $email,$contraseniaUsuario) {
-        return $this->conexion->insert("INSERT INTO Usuario (nombre, apellido, nombreUsuario, correo, contrasenia) 
-                VALUES ('$nombre', '$apellido', '$usuario', '$email', '$contraseniaUsuario')");
+    public function registrar($formularioDeRegistro) {
 
+        $nombre = $formularioDeRegistro->getNombre();
+        $apellido = $formularioDeRegistro->getApellido();
+        $nombreUsuario = $formularioDeRegistro->getNombreUsuario();
+        $email = $formularioDeRegistro->getEmail();
+        $contraseniaEncriptada = md5($formularioDeRegistro->getPassword());
+
+        if ($this->validarEmailDisponible($email)) {
+
+            $idUsuario = $this->conexion->insert("INSERT INTO Usuario (nombre, apellido, nombreUsuario, correo, contrasenia) 
+                VALUES ('$nombre', '$apellido', '$nombreUsuario', '$email', '$contraseniaEncriptada')");
+
+            $resultado = $this->conexion->query("SELECT id FROM Rol WHERE descripcion = 'Lector'");
+            $idRol = $resultado[0]['id'];
+            $this->conexion->insert("INSERT INTO RolUsuario (idRol, idUsuario) 
+            VALUES ($idRol, $idUsuario)");
+
+        }   else {
+            throw new EmailEnUsoException();
+        }
+    }
+
+    private function validarEmailDisponible($email) {
+        $resultado = $this->conexion->query("SELECT COUNT(*) AS usuariosConEmail FROM Usuario WHERE correo = '$email'");
+        return $resultado[0]['usuariosConEmail'] == 0;
     }
 }
 ?>

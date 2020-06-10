87d6b9aa35ae028ef602c24df55ce70c780d12a0
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-10 09:31:24 -0300

    Se crean excepciones para el usuario que ingresa un mail que ya esta en uso

---
+++ app/excepciones/EmailEnUsoException.php
@@ -0,0 +1,6 @@
+<?php
+class EmailEnUsoException extends Exception  {
+    function __construct($message = "", $code = 0, Throwable $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}

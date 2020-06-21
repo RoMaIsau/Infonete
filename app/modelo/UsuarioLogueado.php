91bbed210508778b7a1034f80a6347d10ef48048
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-20 23:08:00 -0300

    Agrega metodos para consultar que rol contiene

--- app/modelo/UsuarioLogueado.php
+++ app/modelo/UsuarioLogueado.php
@@ -22,8 +22,27 @@ class UsuarioLogueado {
         return $this->apellido;
     }
 
+    public function correo() {
+        return $this->correo;
+    }
+
     public function roles() {
         return $this->roles;
+    }
+
+    public function esAdministrador() {
+        return $this->tieneRol("ADMINISTRADOR");
+    }
+
+    public function esLector() {
+        return $this->tieneRol("LECTOR");
+    }
+
+    private function tieneRol($rolBuscado) {
+        $resultado = array_filter($this->roles, function ($valor) use ($rolBuscado) {
+           return $rolBuscado == $valor;
+        });
+        return !empty($resultado);
     }
 }
 ?>

489a9acaa98f20e1deac7319979d68785478bb2e
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-21 20:38:46 -0300

    Guarda una referencia al renderizador porque todos los controllers lo necesitan

--- app/controladores/ControladorBasico.php
+++ app/controladores/ControladorBasico.php
@@ -1,9 +1,11 @@
 <?php
 include_once ("$_SERVER[DOCUMENT_ROOT]/modelo/UsuarioLogueado.php");
+include_once ("$_SERVER[DOCUMENT_ROOT]/helper/Mapeador.php");
 abstract class ControladorBasico {
 
     protected $data;
     protected $usuarioLogueado;
+    protected $renderizador;
 
     public function preAccion() {
         if (isset($_SESSION['usuario'])) {

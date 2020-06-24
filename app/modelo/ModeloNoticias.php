471f040563a1b7743e7dc4dc3e35579087de9dc5
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-23 22:03:40 -0300

    Obtiene la vista previa de todas la noticias de una edicion

--- app/modelo/ModeloNoticias.php
+++ app/modelo/ModeloNoticias.php
@@ -1,7 +1,20 @@
 <?php
+class ModeloNoticias {
+    private $conexion;
 
+    public function __construct($baseDeDatos) {
+        $this->conexion = $baseDeDatos;
+    }
 
-class ModeloNoticias
-{
-
+    public function obtenerVistaPreviaDeNoticiasPorEdicion($idEdicion) {
+        $vistaPrevia = array();
+        $noticias = $this->conexion->query(Consultas::OBTENER_NOTICIAS_POR_EDICION($idEdicion));
+        for ($i = 0; $i < count($noticias); $i++) {
+            $noticia = $noticias[$i];
+            $imagen = $this->conexion->obtenerUnUnicoResultado(Consultas::OBTENER_PRIMER_IMAGEN_DE_NOTICIA($noticia['id']));
+            array_push($vistaPrevia, new VistaPreviaNoticia($noticia['id'], $noticia['titulo'], $noticia['seccion'], $imagen['ubicacion']));
+        }
+        return $vistaPrevia;
+    }
 }
+?>

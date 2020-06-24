c25b730810edaef53000ae8b38d205de59573d38
Rocio Isaurralde <rocio.isau@gmail.com>
2020-06-23 23:33:45 -0300

    Modela los distintos estados de una edicion

---
+++ app/modelo/EstadoEdicion.php
@@ -0,0 +1,16 @@
+<?php
+class EstadoEdicion {
+    public static function EN_PROCESO() {
+        return "EN_PROCESO";
+    }
+    public static function PENDIENTE() {
+        return "PENDIENTE";
+    }
+    public static function PUBLICADA() {
+        return "PUBLICADA";
+    }
+    public static function RECHAZADA() {
+    return "RECHAZADA";
+    }
+}
+?>
